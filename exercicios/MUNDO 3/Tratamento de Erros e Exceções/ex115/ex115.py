1a7b0bb642f0db426d2595e4b33d0597aa6775cc
maycon <maycon5591@gmail.com>
2021-09-18 17:18:39 -0300

    att

--- exercicios/MUNDO 3/Tratamento de Erros e Exceções/ex115/ex115.py
+++ exercicios/MUNDO 3/Tratamento de Erros e Exceções/ex115/ex115.py
@@ -1,20 +1,20 @@
-from utilidades import string_ut
+from utilidades import interface
 from time import sleep
 
 while True:
-      string_ut.menu_([{'1': 'Ver pessoas cadastradas'}, {'2': 'Cadastrar nova pessoa'}, {'3': 'Sair do Sistema'}])
+      interface.menu_([{'1': 'Ver pessoas cadastradas'}, {'2': 'Cadastrar nova pessoa'}, {'3': 'Sair do Sistema'}])
       while True:
-            num = string_ut.leiaInt(f'{string_ut.color(3)}Sua opção: ')
+            num = interface.leiaInt(f'{interface.color(3)}Sua opção: ')
             if num > 3:
-                  print(f'{string_ut.color(1)}Essa opção não existe.')
+                  print(f'{interface.color(1)}Essa opção não existe.')
             else:
                   break
       if num == 1:
-            string_ut.title(f'OPÇÃO {num}')
+            interface.title(f'OPÇÃO {num}')
       elif num == 2:
-            string_ut.title(f'OPÇÃO {num}')
+            interface.title(f'OPÇÃO {num}')
       else:
             break
       sleep(1)
-string_ut.title('Saindo do sistema... Até logo!')
+interface.title('Saindo do sistema... Até logo!')
 
